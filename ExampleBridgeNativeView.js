42003ce386e5eb7e70549d7abfdbb2d1cad6a45f
Chi-AnTai <andy.chian.tai@gmail.com>
2018-09-24 13:16:33 +0800

    change dependency react native to version 0.55.2, create example bridge

---
+++ ExampleBridgeNativeView.js
@@ -0,0 +1,16 @@
+//  Created by react-native-create-bridge
+
+import React, { Component } from 'react'
+import { requireNativeComponent } from 'react-native'
+
+const ExampleBridge = requireNativeComponent('ExampleBridge', ExampleBridgeView)
+
+export default class ExampleBridgeView extends Component {
+  render () {
+    return <ExampleBridge {...this.props} />
+  }
+}
+
+ExampleBridgeView.propTypes = {
+  exampleProp: React.PropTypes.string
+}
